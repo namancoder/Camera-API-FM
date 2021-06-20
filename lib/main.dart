296bc2dedb76517d971db1cc21e56407e66a8f7b
namancoder <namanitsofficial@gmail.com>
2021-06-21 01:01:02 +0530

    v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,39 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:http/http.dart' as http;
 
-Future<List<User>> fetchpp(http.Client client) async {
-  final response =
-      await client.get(Uri.parse('https://fakeface.rest/face/json'));
+Future<User> _getUsers() async {
+  http.Response response =
+      await http.get(Uri.parse("https://fakeface.rest/face/json"));
 
-  return compute(parser, response.body);
+  if (response.statusCode == 200) {
+    print("CoNECCTIoN SECURED \n");
+    print(response.body);
+    return User.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception("Failed to load");
+  }
 }
 
-List<User> parser(String responseBody) {
-  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
-
-  return parsed.map<User>((json) => User.fromJson(json)).toList();
-}
+late final String img;
 
 class User {
   final int age;
   final String gender;
-  final String imageUrl;
+  final String image_url;
 
   User({
     required this.age,
     required this.gender,
-    required this.imageUrl,
+    required this.image_url,
   });
-
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
-      age: json['age'] as int,
-      gender: json['title'] as String,
-      imageUrl: json['url'] as String,
+      age: json['age'],
+      gender: json['gender'],
+      image_url: json['image_url'],
     );
   }
 }
@@ -54,9 +54,12 @@ class CameraApp extends StatefulWidget {
 
 class _CameraAppState extends State<CameraApp> {
   late CameraController controller;
+  late Future<User> futureUsers;
+
   @override
   void initState() {
     super.initState();
+    futureUsers = _getUsers();
     controller = CameraController(cameras[0], ResolutionPreset.max);
     controller.initialize().then((_) {
       if (!mounted) {
@@ -78,6 +81,7 @@ class _CameraAppState extends State<CameraApp> {
       return Container();
     }
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
@@ -85,97 +89,112 @@ class _CameraAppState extends State<CameraApp> {
         leading: Icon(Icons.arrow_back, color: Colors.black),
         title: Text("Savings", style: TextStyle(color: Colors.black)),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+      body: Stack(
         children: [
-          Align(
-            child: Text(
-              "Pay Through UPI",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0),
-            ),
-            alignment: Alignment.centerLeft,
-            widthFactor: 2.9,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10.0, left: 15.0, right: 15.0),
-            child: TextField(
-              decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(2),
-                  border: UnderlineInputBorder(),
-                  hintText: 'Enter UPI Number'),
-            ),
-          ),
-          // Transform.scale(
-          //   scale: 1,
-          //   child: Center(
-          //     child: AspectRatio(
-          //       aspectRatio: 1,
-          //       child: CameraPreview(controller),
-          //     ),
-          //   ),
-          // ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Align(
+                child: Text(
+                  "Pay Through UPI",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0),
+                ),
+                alignment: Alignment.centerLeft,
+                widthFactor: 2.9,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.only(top: 10.0, left: 15.0, right: 15.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(2),
+                      border: UnderlineInputBorder(),
+                      hintText: 'Enter UPI Number'),
+                ),
+              ),
+                            SizedBox(height: 40.0),
 
-          SizedBox(height: 75.0),
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.blueAccent),
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      suffixIcon: Icon(Icons.book),
-                      //contentPadding: EdgeInsets.only(top: 0, left: 25),
-                      hintText: 'Select Number',
-                      hintStyle: TextStyle(fontSize: 17.0),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: BorderSide(color: Colors.transparent),
-                      ),
+              Transform.scale(
+                scale: 1,
+                child: Center(
+                  child: AspectRatio(
+                    aspectRatio: 1,
+                    child: CameraPreview(controller),
+                  ),
+                ),
+              ),
+              SizedBox(height: 75.0),
+            ],
+          ),
+          DraggableScrollableSheet(
+              initialChildSize: 0.2,
+              minChildSize: 0.13,
+              maxChildSize: 0.5,
+              builder: (BuildContext context, s) {
+                return Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue[100],
+                      borderRadius: BorderRadius.only( 
+                      topRight: Radius.circular(40),
+                      topLeft: Radius.circular(40),
+                      )
                     ),
+                    child: FutureBuilder<User>(
+                        future: futureUsers,
+                        builder:
+                            (BuildContext context, AsyncSnapshot snapshot) {
+                          return ListView.builder(
+                              padding: EdgeInsets.all(8.0),
+                              controller: s,
+                              itemCount: 5,
+                              itemBuilder: (BuildContext context, int index) {
+                                if (index == 0) {
+                                  //img = snapshot.data!.image_url;
+                                  return Column(
+                                    children: [Padding(child: Text("Search Contact" ,style:TextStyle(color: Colors.black, fontWeight: FontWeight.bold,  ),),padding: EdgeInsets.all(10.0),  ),
+                                      TextField(
+                                        decoration: InputDecoration(
+                                          suffixIcon: Icon(Icons.book ),
+                                          contentPadding: EdgeInsets.only(
+                                              top: 15.0, left: 10.0),
+                                          hintText: 'Select Number',
+                                          hintStyle: TextStyle(fontSize: 17.0),
+                                          focusedBorder: OutlineInputBorder(
+                                            borderRadius: BorderRadius.circular(20),
+                                            borderSide: BorderSide(
+                                                color: Colors.transparent),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  );
+                                } else {
+                                  if (snapshot.hasData) {
+                                    return ListTile(
+                                        contentPadding: EdgeInsets.all(8.0),
+                                        title: Text("Naman Gupta"),
+                                        subtitle: Text(snapshot.data.gender),
+                                        leading: CircleAvatar(
+                                          backgroundImage: NetworkImage(
+                                              snapshot.data!.image_url),
+                                          radius: 30.0,
+                                        ));
+                                  } else
+                                    return Container(
+                                      child: Center(
+                                        child: Text("LOADING ......."),
+                                      ),
+                                    );
+                                }
+                              });
+                        }),
                   ),
-                ),
-                Container(
-                  child: FutureBuilder<List<User>>(
-                    future: fetchpp(http.Client()),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasError) print(snapshot.error);
-                      if (snapshot.hasData) print("FJHSBZFHBJHCBJHCBHSHJV");
-
-                      return snapshot.hasData
-                          ? UserList(photos: snapshot.data!)
-                          : Center(child: CircularProgressIndicator());
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ),
+                );
+              }),
         ],
       ),
-    );
-  }
-}
-
-class UserList extends StatelessWidget {
-  final List<User> photos;
-
-  UserList({Key? key, required this.photos}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: photos.length,
-      itemBuilder: (context, index) {
-        return Column(children: [
-          Text("$photos[index].age"),
-          Text(photos[index].imageUrl),
-          Text(photos[index].gender)
-        ]);
-      },
     );
   }
 }
